b99ff5aa6ad25205219de87d919a892cf2223713
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-03-28 09:56:49 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webjure/jeesql "0.2.2"
+(defproject webjure/jeesql "0.2.3"
   :description "A Clojure library for using SQL"
   :url "https://github.com/tatut/jeesql"
   :license {:name "Eclipse Public License"
